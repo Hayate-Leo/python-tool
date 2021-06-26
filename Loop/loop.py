7b0e8bd6c8984e9fb41f78019f9d289bbfa1d51f
Hayate-Leo <fumiaki0213@icloud.com>
2021-06-26 16:27:43 +0900

    loop_enu

--- Loop/loop.py
+++ Loop/loop.py
@@ -7,8 +7,8 @@ list_energy = [0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.1,
 d = {'Kair': list_a, 'Energy': list_energy}
 
 def loop_dict():
-    for key, list_value in d.items():
-        print(key)
+    for i, (key, list_value) in enumerate(d.items()):
+        print(i, key)
         for value in list_value:
             print(value)
 
