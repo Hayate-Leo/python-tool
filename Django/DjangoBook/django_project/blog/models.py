19341ce7d891d193a505598fc818941ae6fd0d8c
Hayate-Leo <fumiaki0213@icloud.com>
2021-10-11 20:13:22 +0900

    お問い合わせフォーム

--- Django/DjangoBook/django_project/blog/models.py
+++ Django/DjangoBook/django_project/blog/models.py
@@ -11,3 +11,15 @@ class Blog(models.Model):
 
     class Meta:
         db_table = 'blogs'
+
+class Contact(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date = models.DateField(auto_now_add=True)
+    email = models.EmailField(max_length=200)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        db_table = 'contacts'
--- Django/DjangoBook/django_project/blog/views.py
+++ Django/DjangoBook/django_project/blog/views.py
@@ -1,4 +1,16 @@
 from django.views.generic import TemplateView
+from django.views.generic.edit import CreateView
+from .forms import ContactForm
+from django.urls import reverse_lazy
+from .models import Contact
+
 
 class HomeTemplateView(TemplateView):
     template_name = 'blog/home.html'
+
+
+class ContactFormView(CreateView):
+    template_name = 'blog/contact.html'
+    form_class = ContactForm
+    success_url = reverse_lazy('home')
+    model = Contact
