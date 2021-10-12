4247cd98f169cba31a0f41f71565843c17b622dd
Hayate-Leo <fumiaki0213@icloud.com>
2021-10-12 09:12:05 +0900

    message機能追加

--- Django/DjangoBook/django_project/blog/views.py
+++ Django/DjangoBook/django_project/blog/views.py
@@ -3,14 +3,16 @@ from django.views.generic.edit import CreateView
 from .forms import ContactForm
 from django.urls import reverse_lazy
 from .models import Contact
+from django.contrib.messages.views import SuccessMessageMixin
 
 
 class HomeTemplateView(TemplateView):
     template_name = 'blog/home.html'
 
 
-class ContactFormView(CreateView):
+class ContactFormView(SuccessMessageMixin, CreateView):
     template_name = 'blog/contact.html'
     form_class = ContactForm
     success_url = reverse_lazy('home')
     model = Contact
+    success_message = 'お問い合わせありがとうございました。'
