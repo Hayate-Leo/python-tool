d150ef4b40d1f08de2461c8653d712befe066c33
Hayate-Leo <fumiaki0213@icloud.com>
2021-10-17 20:40:25 +0900

    choiceフィールド

--- Django/DjangoBook/django_project/blog/models.py
+++ Django/DjangoBook/django_project/blog/models.py
@@ -1,8 +1,15 @@
 from django.db import models
 
 class Blog(models.Model):
+    LANG_TYPES = (
+        ('py', 'Python'),
+        ('js', 'Javascript'),
+        ('css', 'CSS'),
+        ('html', 'HTML'),
+    )
     title = models.CharField(max_length=200)
     content = models.TextField(blank=True)
+    lang_type = models.CharField(max_length=50, choices=LANG_TYPES, blank=True)
     updated_at = models.DateTimeField(auto_now=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
